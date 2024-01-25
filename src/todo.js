819caecda900d139aff2a6bd92d407a981f99cbe
Joshua Allen <batayolajoshuaallen@gmail.com>
2024-01-25 14:28:18 +0800

    created for handler and project to use

---
+++ src/todo.js
@@ -0,0 +1,11 @@
+class Todo {
+  constructor(id, name,date,isPriority, projectName) {
+    this.id = id;
+    this.name = name
+    this.date = date;
+    this.isPriority = isPriority;
+    this.projectName = projectName;
+  }
+}
+
+export default Todo;
