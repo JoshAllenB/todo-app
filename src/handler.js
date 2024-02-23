aaf4587618adce6040f4ee121843a762914122f0
Joshua Allen <batayolajoshuaallen@gmail.com>
2024-02-23 21:25:50 +0800

    refactor code for modularity

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,6 @@
 class Handler {
   constructor() {
-    this.todos = [];
+    this.todo = [];
     this.todoIdCounter = 0;
     this.projectName = "";
     this.selectedProject = null;
@@ -14,51 +14,6 @@ class Handler {
     return button;
   }
 
-  createTodoListForm(todoContainer) {
-    const form = document.createElement("form");
-    form.classList.add("todo-form");
-
-    const nameInput = this.createInput("text", "Todo Name", true);
-    const dateInput = this.createInput("date", "", true);
-
-    const addButton = this.createButton("Add Todo", "addBtn");
-    addButton.addEventListener("click", (event) => {
-      event.preventDefault();
-
-      const name = nameInput.value;
-      const date = dateInput.value;
-
-      if (name && date) {
-        this.addTodoToInbox(name, date);
-        const todo = {
-          id: this.todoIdCounter++,
-          name,
-          date,
-          isPriority: false,
-        };
-
-        const todoItem = this.createTodoItem(todo, todoContainer);
-
-        if (todo.isPriority) {
-          todoItem.classList.add("priority");
-        }
-
-        if (todoContainer) {
-          todoContainer.appendChild(todoItem);
-        }
-
-        nameInput.value = "";
-        dateInput.value = "";
-      }
-    });
-
-    form.appendChild(nameInput);
-    form.appendChild(dateInput);
-    form.appendChild(addButton);
-
-    return form;
-  }
-
   createInput(type, placeholder, required) {
     const input = document.createElement("input");
     input.type = type;
@@ -68,37 +23,18 @@ class Handler {
     return input;
   }
 
-  createTodoItem(todo, inboxContainer) {
-    const todoItem = document.createElement("div");
-    todoItem.classList.add("todo-item");
-    todoItem.classList.add(`todo-id-${todo.id}`);
-    todoItem.setAttribute("data-todo-id", todo.id);
+  createTodoContainer() {
+    const container = document.createElement("div");
+    container.classList.add("todoContainer");
 
-    console.log("Creating todo item:", todo);
+    return container;
+  }
 
-    const checkboxContainer = this.createCheckboxContainer(todo);
+  createItemContainer() {
+    const itemContainer = document.createElement("div");
+    itemContainer.classList.add("itemContainer");
 
-    const nameLabel = document.createElement("span");
-    nameLabel.textContent = todo.name;
-
-    const dateLabel = document.createElement("span");
-    dateLabel.textContent = todo.date;
-    console.log("Date Label Value:", dateLabel.textContent);
-
-    const priorityIcon = this.createPriorityIcon(
-      todo,
-      todoItem,
-      inboxContainer
-    );
-
-    todoItem.appendChild(checkboxContainer);
-    todoItem.appendChild(nameLabel);
-    todoItem.appendChild(dateLabel);
-    todoItem.appendChild(priorityIcon);
-
-    console.log("Todo Item created:", todoItem);
-
-    return todoItem;
+    return itemContainer;
   }
 
   createCheckboxContainer(todo) {
@@ -141,10 +77,9 @@ class Handler {
 
       this.updatePriorityIcon(todo);
 
-      if (todo.isPriority) {
+      if (todo.isPriority && inboxContainer) {
         todoItem.classList.add("priority");
         inboxContainer.prepend(todoItem);
-        console.log("Priority!", this.todos);
       }
     });
 
@@ -170,37 +105,22 @@ class Handler {
     });
   }
 
-  addTodoToInbox(name, date) {
-    const todoItemContainer = document.querySelector(".todo-item-container");
-
-    const todo = {
-      id: this.todoIdCounter++,
-      name,
-      date,
-      isPriority: false,
-    };
-
-    this.todos.push(todo);
-
-    console.log("Current Todos:", this.todos);
-
-    const todoItem = this.createTodoItem(todo, todoItemContainer);
-
-    if (todo.isPriority) {
-      todoItem.classList.add("priority");
-    }
-    todoItemContainer.appendChild(todoItem);
-
-    this.arrangeInbox(todoItemContainer);
-  }
-
   removeTodoFromList(todo) {
-    const index = this.todos.indexOf(todo);
+    const index = this.todo.indexOf(todo);
     if (index !== -1) {
-      this.todos.splice(index, 1);
+      this.todo.splice(index, 1);
       const todoItem = document.querySelector(`.todo-id-${todo.id}`);
       if (todoItem) {
         todoItem.remove();
+      }
+    }
+  }
+
+  clearList(container) {
+    const itemContainer = container.querySelector(".itemContainer");
+    if (itemContainer) {
+      while (itemContainer.firstChild) {
+        itemContainer.removeChild(itemContainer.firstChild);
       }
     }
   }
@@ -230,7 +150,7 @@ class Handler {
           59
         );
 
-    return this.todos.filter((todo) => {
+    return this.todo.filter((todo) => {
       const todoDueDate = new Date(todo.date);
 
       if (includeTime) {
@@ -238,49 +158,6 @@ class Handler {
       } else {
         return todoDueDate >= startOfDate && todoDueDate <= endOfDate;
       }
-    });
-  }
-
-  arrangeInbox(inboxContainer) {
-    const todoItems = Array.from(
-      inboxContainer.getElementsByClassName("todo-item")
-    );
-
-    const regularTodos = todoItems.filter(
-      (todoItem) => !todoItem.classList.contains("priority")
-    );
-    const priorityTodos = todoItems.filter((todoItem) =>
-      todoItem.classList.contains("priority")
-    );
-
-    const sortDate = (a, b) => {
-      const dateA = new Date(a.querySelector("span:nth-child(3)").textContent);
-      const dateB = new Date(b.querySelector("span:nth-child(3)").textContent);
-      return dateA - dateB;
-    };
-
-    priorityTodos.sort((a, b) => {
-      const dateA = new Date(a.querySelector("span:nth-child(3)").textContent);
-      const dateB = new Date(b.querySelector("span:nth-child(3)").textContent);
-
-      if (dateA.getTime() === dateB.getTime()) {
-        const priorityA = a.classList.contains("priority") ? 1 : 0;
-        const priorityB = b.classList.contains("priority") ? 1 : 0;
-        return priorityB - priorityA;
-      }
-
-      return dateA - dateB;
-    });
-
-    regularTodos.sort(sortDate);
-    inboxContainer.innerHTML = "";
-
-    priorityTodos.forEach((todoItem) => {
-      inboxContainer.appendChild(todoItem);
-    });
-
-    regularTodos.forEach((todoItem) => {
-      inboxContainer.appendChild(todoItem);
     });
   }
 }
